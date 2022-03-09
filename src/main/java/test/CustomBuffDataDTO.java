aa7bd778725c36eb1612cc45ee7ab28e176822e9
Jessefjxm <jessefjxm@gmail.com>
2022-03-09 20:00:38 +0800

    fastjson

---
+++ src/main/java/test/CustomBuffDataDTO.java
@@ -0,0 +1,10 @@
+package test;
+
+import java.util.HashMap;
+
+public class CustomBuffDataDTO {
+    public int skillId, time;
+    public boolean isItem;
+    public String name;
+    public HashMap<String, Integer> statups = new HashMap<>();
+}
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,10 +1,32 @@
 package test;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 public class Test {
 
     public static void main(String[] args) {
+        String path = "CustomBuffDataDTO.json";
+        CustomBuffDataDTO dto = new CustomBuffDataDTO();
+        dto.isItem = true;
+        dto.skillId = 1;
+        dto.name = "test";
+        try {
+            FileUtils.writeStringToFile(new File(path), JSON.toJSONString(dto), StandardCharsets.UTF_8);
+            String str = FileUtils.readFileToString(new File(path), StandardCharsets.UTF_8);
+            CustomBuffDataDTO read = JSON.parseObject(str, CustomBuffDataDTO.class);
+            System.out.println(JSON.toJSONString(read));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void kryo() {
         String path = "name.ser";
         SerializedName serializedName = new SerializedName();
         serializedName.faceNames = new HashMap<>();
